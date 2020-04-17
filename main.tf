4ed80de76efd0fad9925058f1db786bd532f9a08
bainss <surjit.bains@gmail.com>
2020-04-17 18:08:04 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "resource-group" {
-  name     = "${var.name}"
-  location = "${var.location}"
-  tags     = "${var.tags}"
+  name     = var.name
+  location = var.location
+  tags     = var.tags
 }
