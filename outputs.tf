a1d2b5c4e65253824ede8faef3fc39607511a245
bainss <surjit.bains@gmail.com>
2020-04-17 18:08:30 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "name" {
-  value       = "${var.name}"
+  value       = var.name
   description = "The Name of this resource group."
 }
 
 output "location" {
-  value       = "${var.location}"
+  value       = var.location
   description = "The Azure Region where the resource group should exist."
 }
